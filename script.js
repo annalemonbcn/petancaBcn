74a05490acdea2a8fef8ac68386bec91f0b7a633
annae <anna.romagosa@smartassets.ai>
2024-11-05 16:22:25 +0100

    markers placed in map + filtered

--- script.js
+++ script.js
@@ -1,62 +1,461 @@
-const markers = [
+const mockMarkers = [
   {
-    name: "Pistes de Petanca la Llosa del Bon Pastor",
-    lat: 41.43685278329868,
-    lng: 2.2090017756302776,
-  },
-  {
-    name: "Pistes Municipals de Petanca Exposició",
-    lat: 41.37122765895013,
-    lng: 2.162878181473128,
-  },
-  {
+    _full_count: "277",
+    addresses_roadtype_name: "",
+    addresses_end_street_number: null,
+    geo_epgs_4326_lon: "2.184595591064207",
+    values_attribute_name: "",
+    estimated_dates: "",
+    addresses_road_name: "C Palamós",
+    values_category: "",
+    addresses_zip_code: "8016",
+    secondary_filters_id: null,
+    values_value: "",
+    addresses_town: "BARCELONA",
+    secondary_filters_name: "",
+    secondary_filters_tree: null,
+    start_date: "",
+    addresses_district_name: "Nou Barris",
+    end_date: "",
+    geo_epgs_25831_x: "431887.32770494267",
+    addresses_start_street_number: "19",
+    register_id: "99400237597",
+    institution_id: null,
+    addresses_main_address: "True",
+    rank: 0.0573088,
+    addresses_district_id: "8",
+    addresses_roadtype_id: "",
+    addresses_type: "",
+    addresses_neighborhood_id: "52",
+    _id: 319,
     name: "Club de Petanca Amics de Nou Barris",
-    lat: 41.44602969483294,
-    lng: 2.184595591064207,
+    addresses_road_id: "241200",
+    created: "2009-12-03T18:34:06",
+    geo_epgs_25831_y: "4588593.335697808",
+    institution_name: "",
+    modified: "2022-09-17T07:17:31.511838",
+    secondary_filters_asia_id: null,
+    secondary_filters_fullpath: "",
+    geo_epgs_4326_lat: "41.44602969483294",
+    values_description: "",
+    values_id: null,
+    addresses_neighborhood_name: "la Prosperitat",
+    values_outstanding: "",
+    values_attribute_id: null,
   },
   {
-    name: "Pistes Municipals de Petanca Sant Joan",
-    lat: 41.40318752777066,
-    lng: 2.1641778235213587,
+    _full_count: "277",
+    addresses_roadtype_name: "",
+    addresses_end_street_number: null,
+    geo_epgs_4326_lon: "2.1415194491303953",
+    values_attribute_name: "No tenen telèfon",
+    estimated_dates: "",
+    addresses_road_name: "Entença",
+    values_category: "Telèfons",
+    addresses_zip_code: "8029",
+    secondary_filters_id: "57732077",
+    values_value: "-",
+    addresses_town: "BARCELONA",
+    secondary_filters_name: "Altres esports",
+    secondary_filters_tree: "651",
+    start_date: "",
+    addresses_district_name: "Les Corts",
+    end_date: "",
+    geo_epgs_25831_x: "428224.0896320879",
+    addresses_start_street_number: "242",
+    register_id: "92086010722",
+    institution_id: null,
+    addresses_main_address: "True",
+    rank: 0.0573088,
+    addresses_district_id: "4",
+    addresses_roadtype_id: "",
+    addresses_type: "",
+    addresses_neighborhood_id: "19",
+    _id: 81,
+    name: "Pistes Municipals de Petanca Entença",
+    addresses_road_id: "110803",
+    created: "1985-02-12T00:00:00",
+    geo_epgs_25831_y: "4582075.6474766545",
+    institution_name: "",
+    modified: "2022-09-17T04:14:20.433991",
+    secondary_filters_asia_id: "65103004001009",
+    secondary_filters_fullpath:
+      "Planol BCN \u003E\u003E Esports \u003E\u003E Esports individuals \u003E\u003E Altres esports",
+    geo_epgs_4326_lat: "41.38700849381915",
+    values_description: "",
+    values_id: "163533",
+    addresses_neighborhood_name: "les Corts",
+    values_outstanding: "True",
+    values_attribute_id: "100050",
+  },
+  {
+    _full_count: "277",
+    addresses_roadtype_name: "",
+    addresses_end_street_number: null,
+    geo_epgs_4326_lon: "2.1871583850708625",
+    values_attribute_name: "",
+    estimated_dates: "",
+    addresses_road_name: "C Malats",
+    values_category: "",
+    addresses_zip_code: "8030",
+    secondary_filters_id: "57345754",
+    values_value: "",
+    addresses_town: "BARCELONA",
+    secondary_filters_name: "Instal·lacions esportives",
+    secondary_filters_tree: "651",
+    start_date: "",
+    addresses_district_name: "Sant Andreu",
+    end_date: "",
+    geo_epgs_25831_x: "432091.3469421068",
+    addresses_start_street_number: "47",
+    register_id: "99400140296",
+    institution_id: null,
+    addresses_main_address: "True",
+    rank: 0.0573088,
+    addresses_district_id: "9",
+    addresses_roadtype_id: "",
+    addresses_type: "",
+    addresses_neighborhood_id: "60",
+    _id: 2033,
+    name: "Pistes de Petanca Bascònia",
+    addresses_road_id: "190303",
+    created: "2005-08-08T15:01:57",
+    geo_epgs_25831_y: "4587520.109192251",
+    institution_name: "",
+    modified: "2022-09-17T06:27:39.787413",
+    secondary_filters_asia_id: "65102004002",
+    secondary_filters_fullpath:
+      "Planol BCN \u003E\u003E Esports \u003E\u003E Instal·lacions esportives",
+    geo_epgs_4326_lat: "41.4363808168405",
+    values_description: "",
+    values_id: null,
+    addresses_neighborhood_name: "Sant Andreu",
+    values_outstanding: "",
+    values_attribute_id: null,
+  },
+  {
+    _full_count: "277",
+    addresses_roadtype_name: "",
+    addresses_end_street_number: null,
+    geo_epgs_4326_lon: "2.135163335313236",
+    values_attribute_name: "Tel.",
+    estimated_dates: "",
+    addresses_road_name: "C Alts Forns",
+    values_category: "Telèfons",
+    addresses_zip_code: "8038",
+    secondary_filters_id: null,
+    values_value: "934222638",
+    addresses_town: "BARCELONA",
+    secondary_filters_name: "",
+    secondary_filters_tree: null,
+    start_date: "",
+    addresses_district_name: "Sants-Montjuïc",
+    end_date: "",
+    geo_epgs_25831_x: "427659.2588413517",
+    addresses_start_street_number: "13",
+    register_id: "﻿99400079142",
+    institution_id: null,
+    addresses_main_address: "True",
+    rank: 0.0573088,
+    addresses_district_id: "3",
+    addresses_roadtype_id: "",
+    addresses_type: "",
+    addresses_neighborhood_id: "13",
+    _id: 9,
+    name: "Club petanca 8 de maig",
+    addresses_road_id: "12600",
+    created: "2004-04-06T15:42:42",
+    geo_epgs_25831_y: "4578732.986990525",
+    institution_name: "",
+    modified: "2023-02-13T18:35:17.112555",
+    secondary_filters_asia_id: null,
+    secondary_filters_fullpath: "",
+    geo_epgs_4326_lat: "41.35685180205606",
+    values_description: "",
+    values_id: "210259",
+    addresses_neighborhood_name: "la Marina de Port",
+    values_outstanding: "True",
+    values_attribute_id: "20001",
+  },
+  {
+    _full_count: "277",
+    addresses_roadtype_name: "",
+    addresses_end_street_number: "4",
+    geo_epgs_4326_lon: "2.1968923181627025",
+    values_attribute_name: "",
+    estimated_dates: "",
+    addresses_road_name: "Carrer de Santander",
+    values_category: "",
+    addresses_zip_code: "8020",
+    secondary_filters_id: null,
+    values_value: "",
+    addresses_town: "Barcelona",
+    secondary_filters_name: "",
+    secondary_filters_tree: null,
+    start_date: "",
+    addresses_district_name: "Sant Martí",
+    end_date: "",
+    geo_epgs_25831_x: "432890.27",
+    addresses_start_street_number: "4",
+    register_id: "﻿92086012713",
+    institution_id: null,
+    addresses_main_address: "True",
+    rank: 0.0573088,
+    addresses_district_id: "10",
+    addresses_roadtype_id: "",
+    addresses_type: "",
+    addresses_neighborhood_id: "73",
+    _id: 484,
+    name: "Club Petanca Montseny",
+    addresses_road_id: "318906",
+    created: "1985-05-08T00:00:00",
+    geo_epgs_25831_y: "4585970.533",
+    institution_name: "",
+    modified: "2023-09-22T11:16:45.339889",
+    secondary_filters_asia_id: null,
+    secondary_filters_fullpath: "",
+    geo_epgs_4326_lat: "41.422491447021095",
+    values_description: "",
+    values_id: null,
+    addresses_neighborhood_name: "la Verneda i la Pau",
+    values_outstanding: "",
+    values_attribute_id: null,
+  },
+  {
+    _full_count: "277",
+    addresses_roadtype_name: "",
+    addresses_end_street_number: null,
+    geo_epgs_4326_lon: "2.205136770475756",
+    values_attribute_name: "",
+    estimated_dates: "",
+    addresses_road_name: "C Pujades",
+    values_category: "",
+    addresses_zip_code: "8005",
+    secondary_filters_id: "57345754",
+    values_value: "",
+    addresses_town: "BARCELONA",
+    secondary_filters_name: "Instal·lacions esportives",
+    secondary_filters_tree: "651",
+    start_date: "",
+    addresses_district_name: "Sant Martí",
+    end_date: "",
+    geo_epgs_25831_x: "433560.596391052",
+    addresses_start_street_number: "270",
+    register_id: "﻿92086010840",
+    institution_id: "99400575441",
+    addresses_main_address: "True",
+    rank: 0.0573088,
+    addresses_district_id: "10",
+    addresses_roadtype_id: "",
+    addresses_type: "",
+    addresses_neighborhood_id: "68",
+    _id: 442,
+    name: "Pistes Municipals de Petanca",
+    addresses_road_id: "270901",
+    created: "1985-02-13T00:00:00",
+    geo_epgs_25831_y: "4583936.721411954",
+    institution_name: "Jardins de Josep Trueta",
+    modified: "2022-09-17T04:14:55.867775",
+    secondary_filters_asia_id: "65102004002",
+    secondary_filters_fullpath:
+      "Planol BCN \u003E\u003E Esports \u003E\u003E Instal·lacions esportives",
+    geo_epgs_4326_lat: "41.40422918556986",
+    values_description: "",
+    values_id: null,
+    addresses_neighborhood_name: "el Poblenou",
+    values_outstanding: "",
+    values_attribute_id: null,
+  },
+  {
+    _full_count: "277",
+    addresses_roadtype_name: "",
+    addresses_end_street_number: null,
+    geo_epgs_4326_lon: "2.1957204550768923",
+    values_attribute_name: "No tenen telèfon",
+    estimated_dates: "",
+    addresses_road_name: "C Andrade",
+    values_category: "Telèfons",
+    addresses_zip_code: "8018",
+    secondary_filters_id: "57732077",
+    values_value: "-",
+    addresses_town: "BARCELONA",
+    secondary_filters_name: "Altres esports",
+    secondary_filters_tree: "651",
+    start_date: "",
+    addresses_district_name: "Sant Martí",
+    end_date: "",
+    geo_epgs_25831_x: "432780.7498103315",
+    addresses_start_street_number: "40",
+    register_id: "﻿98161173959",
+    institution_id: "98161172951",
+    addresses_main_address: "True",
+    rank: 0.0573088,
+    addresses_district_id: "10",
+    addresses_roadtype_id: "",
+    addresses_type: "",
+    addresses_neighborhood_id: "65",
+    _id: 467,
+    name: "Pistes de Petanca",
+    addresses_road_id: "15201",
+    created: "1998-06-10T00:00:00",
+    geo_epgs_25831_y: "4584722.836592159",
+    institution_name: "Complex Esportiu Municipal Clot de la Mel",
+    modified: "2022-09-17T05:40:23.405539",
+    secondary_filters_asia_id: "65103004001009",
+    secondary_filters_fullpath:
+      "Planol BCN \u003E\u003E Esports \u003E\u003E Esports individuals \u003E\u003E Altres esports",
+    geo_epgs_4326_lat: "41.4112446697827",
+    values_description: "",
+    values_id: "200387",
+    addresses_neighborhood_name: "el Clot",
+    values_outstanding: "True",
+    values_attribute_id: "100050",
+  },
+  {
+    _full_count: "277",
+    addresses_roadtype_name: "",
+    addresses_end_street_number: null,
+    geo_epgs_4326_lon: "2.1957204550768923",
+    values_attribute_name: "No tenen telèfon",
+    estimated_dates: "",
+    addresses_road_name: "C Andrade",
+    values_category: "Telèfons",
+    addresses_zip_code: "8018",
+    secondary_filters_id: "57345754",
+    values_value: "-",
+    addresses_town: "BARCELONA",
+    secondary_filters_name: "Instal·lacions esportives",
+    secondary_filters_tree: "651",
+    start_date: "",
+    addresses_district_name: "Sant Martí",
+    end_date: "",
+    geo_epgs_25831_x: "432780.7498103315",
+    addresses_start_street_number: "40",
+    register_id: "﻿98161173959",
+    institution_id: "98161172951",
+    addresses_main_address: "True",
+    rank: 0.0573088,
+    addresses_district_id: "10",
+    addresses_roadtype_id: "",
+    addresses_type: "",
+    addresses_neighborhood_id: "65",
+    _id: 468,
+    name: "Pistes de Petanca",
+    addresses_road_id: "15201",
+    created: "1998-06-10T00:00:00",
+    geo_epgs_25831_y: "4584722.836592159",
+    institution_name: "Complex Esportiu Municipal Clot de la Mel",
+    modified: "2022-09-17T05:40:23.405539",
+    secondary_filters_asia_id: "65102004002",
+    secondary_filters_fullpath:
+      "Planol BCN \u003E\u003E Esports \u003E\u003E Instal·lacions esportives",
+    geo_epgs_4326_lat: "41.4112446697827",
+    values_description: "",
+    values_id: "200387",
+    addresses_neighborhood_name: "el Clot",
+    values_outstanding: "True",
+    values_attribute_id: "100050",
   },
 ];
 
-function getLocationFromArray(position) {
-  const minValue = 0;
-  const maxValue = markers.length - 1;
+const dataToMarkers = (data) =>
+  data.map((marker) => ({
+    name: marker.name,
+    id: marker._id,
+    register_id: marker.register_id,
+    coordinates: {
+      lat: parseFloat(marker.geo_epgs_4326_lat),
+      long: parseFloat(marker.geo_epgs_4326_lon),
+    },
+    address: {
+      district_id: marker.addresses_district_id,
+      district_name: marker.addresses_district_name,
+      road_name: marker.addresses_road_name,
+      street_number: marker.addresses_start_street_number,
+      zip_code: `0${marker.addresses_zip_code}`,
+    },
+    created: marker.created,
+  }));
 
-  if (position > maxValue || position < minValue) {
-    console.alert("Position does not exist in array");
-    return;
-  }
+const filterMarkersByDistrict = (district, markers) => {
+  if (district === "All") return markers;
+  return markers.filter((marker) => marker.address.district_name === district);
+};
 
-  return markers[position];
-}
+const createOption = (selector, value, text = value) => {
+  const option = document.createElement("option");
+  option.value = value;
+  option.innerText = text;
+  selector.appendChild(option);
+};
+
+const makeDistrictSelectorOptions = (markers) => {
+  const selector = document.getElementById("districtsSelector");
+  createOption(selector, "All", "All districts");
+
+  const selectorOptions = [
+    ...new Set(markers.map((marker) => marker.address.district_name)),
+  ];
+  selectorOptions.map((option) => createOption(selector, option));
+};
+
+// Map data from db to easy-reading shape
+const markers = dataToMarkers(mockMarkers);
+
+// Get selector options + create the options
+const selector = document.getElementById("districtsSelector");
+makeDistrictSelectorOptions(markers);
+
+// Get filtered markers
+let filteredMarkers = filterMarkersByDistrict(selector.value || "All", markers);
+
+// Update the map when the selector value changes
+selector.onchange = () => {
+  filteredMarkers = filterMarkersByDistrict(selector.value || "All", markers);
+  updateMap(filteredMarkers);
+};
+
+
+let map;
+let markersArray = [];
 
 function initMap() {
-  alert('Welcome to petancaBCN!')
-  const initLocation = getLocationFromArray(0);
-
-  const map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12,
-    center: { lat: initLocation.lat, lng: initLocation.lng },
+    center: {
+      lat: filteredMarkers[5].coordinates.lat,
+      lng: filteredMarkers[5].coordinates.long,
+    },
     mapTypeControl: false,
   });
 
-  for (let i = 0; i < markers.length; i++) {
-    const currMarker = markers[i];
+  filteredMarkers = filterMarkersByDistrict(selector.value || "All", markers);
+  updateMap(filteredMarkers);
+}
 
-    const marker = new google.maps.Marker({
-      position: { lat: markers[i].lat, lng: markers[i].lng },
+const updateMap = (markers) => {
+  // Clear existing markers
+  markersArray.forEach((marker) => marker.setMap(null));
+  markersArray = [];
+
+  // Add new markers
+  markers.forEach((marker) => {
+    const currMarker = new google.maps.Marker({
+      position: {
+        lat: marker.coordinates.lat,
+        lng: marker.coordinates.long,
+      },
       map: map,
     });
 
     const infoWindow = new google.maps.InfoWindow({
-      content: markers[i].name,
+      content: marker.name,
     });
 
-    marker.addListener("click", () => {
-      infoWindow.open(map, marker);
+    currMarker.addListener("click", () => {
+      infoWindow.open(map, currMarker);
     });
-  }
-}
+
+    markersArray.push(currMarker);
+  });
+};
