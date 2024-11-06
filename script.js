f5584883f335bb90fd4c37aad96ae78ddf4a88d0
annae <anna.romagosa@smartassets.ai>
2024-11-06 15:29:31 +0100

    added street view window

--- script.js
+++ script.js
@@ -374,7 +374,6 @@ const dataToMarkers = (data) =>
       street_number: marker.addresses_start_street_number,
       zip_code: `0${marker.addresses_zip_code}`,
     },
-    created: marker.created,
   }));
 
 const filterMarkersByDistrict = (district, markers) => {
@@ -421,6 +420,7 @@ selector.onchange = () => {
 
 let map;
 let markersArray = [];
+let openInfoWindow = undefined;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -451,14 +451,65 @@ const updateMap = (markers) => {
       map: map,
     });
 
+    const markerAddress = `${marker.address.road_name}, ${marker.address.street_number}, ${marker.address.zip_code}, ${marker.address.district_name}`;
+
+    const contentString =
+      '<div id="infoWindow">' +
+      `<h3 id="firstHeading" class="firstHeading">${marker.name}</h3>` +
+      '<div id="infoWindow-body">' +
+      '<div id="infoWindow-body-address">' +
+      '<p id="address-title">Address:</p>' +
+      `<p id="address-body">${markerAddress}</p>` +
+      "</div>" +
+      '<div id="street-view-container" style="width: 100%; height: 150px;"></div>' +
+      "</div>" +
+      `<div><a href="https://www.google.com/maps/dir/?api=1&destination=${markerAddress}" target="_blank">Take me there</a></div>` +
+      "</div>";
+
     const infoWindow = new google.maps.InfoWindow({
-      content: marker.name,
+      content: contentString,
+      maxWidth: 400,
     });
 
     currMarker.addListener("click", () => {
+      if (openInfoWindow) openInfoWindow.close();
+
       infoWindow.open(map, currMarker);
+      openInfoWindow = infoWindow;
+    });
+
+    google.maps.event.addListenerOnce(infoWindow, "domready", () => {
+      initializeStreetView(marker.coordinates.lat, marker.coordinates.long);
     });
 
     markersArray.push(currMarker);
   });
+};
+
+const initializeStreetView = (lat, lng) => {
+  const streetViewService = new google.maps.StreetViewService();
+
+  // Check if Street View imagery is available at the location
+  streetViewService.getPanorama(
+    { location: { lat, lng }, radius: 50 },
+    (data, status) => {
+      if (status === google.maps.StreetViewStatus.OK) {
+        // Create a Street View panorama
+        const panorama = new google.maps.StreetViewPanorama(
+          document.getElementById("street-view-container"),
+          {
+            position: { lat, lng },
+            pov: {
+              heading: 90,
+              pitch: 10,
+            },
+            addressControl: false,
+          }
+        );
+      } else {
+        document.getElementById("street-view-container").innerHTML =
+          "No Street View available";
+      }
+    }
+  );
 };
