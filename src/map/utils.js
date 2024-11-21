71dac21e4f67fef5767a0eea7862ea0892f8a701
annae <anna.romagosa@smartassets.ai>
2024-11-21 10:43:33 +0100

    toast library added

--- src/map/utils.js
+++ src/map/utils.js
@@ -12,14 +12,24 @@ const filterMarkersByDistrict = (district, markers) => {
 const toggleFavourite = (fav) => {
   const favourites = getAllFavourites();
 
+  let notyf = new Notyf({
+    duration: 3000,
+    position: {
+      x: "right",
+      y: "top",
+    },
+  });
+
   // if already in favs --> delete from localStorage
   if (favourites.find((marker) => marker.id === fav.id)) {
     removeFavourite(fav.id);
+    notyf.success("Removed from favourites");
   }
   // if not in favs --> add localStorage
   else {
     const favString = JSON.stringify(fav);
     localStorage.setItem(`${FAVORITES_KEY_PREFIX}${fav.id}`, favString);
+    notyf.success("Added to favourites");
   }
 };
 
